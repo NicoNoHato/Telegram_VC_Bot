7f4703d08c61b913e5d5b850c5d302147d4010f0
NicoNoHato <79739155+NicoNoHato@users.noreply.github.com>
2021-04-15 05:21:26 +0700

    Update and rename sample_config.py to config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+API_ID = 3182938
+API_HASH = "d5f89c0d9759f6b3b0f0ffddfbbdb541"
+SUDO_CHAT_ID = -1001204586138  # Chat where the bot will play the music.
+SUDOERS = [1457083910, 13216546] # Users which have special control over the bot.
+
+# don't make changes below this line
+ARQ_API = "http://35.240.133.234:8000"
