67c177424a18bd794942f6f9e524da44010921fe
NicoNoHato <79739155+NicoNoHato@users.noreply.github.com>
2021-04-16 01:43:43 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 API_ID = 3182938
 API_HASH = "d5f89c0d9759f6b3b0f0ffddfbbdb541"
-SUDO_CHAT_ID = -1001204586138  # Chat where the bot will play the music.
+SUDO_CHAT_ID = -1001154149817 # Chat where the bot will play the music.
 SUDOERS = [1457083910, 13216546] # Users which have special control over the bot.
 
 # don't make changes below this line
